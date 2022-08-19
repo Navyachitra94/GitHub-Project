5444ef288e4c521c0cf7576b79a7eef0a0ad3b61
Navyachitra <navya19794@gmail.com>
2022-08-19 18:35:19 +0530

    added/deleted files

---
+++ src/Pages/SearchPage.java
@@ -0,0 +1,13 @@
+package Pages;
+
+public class SearchPage {
+	
+	public static void main (String a[]) {
+		System.out.println("Search page");
+		String user = "admin";
+		String pwd = "admin1";
+		System.out.println("Logged in");
+		
+	}
+
+}
