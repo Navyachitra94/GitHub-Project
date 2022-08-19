47b364b04782cf332ad2b37795ab13ecdb2f362f
Navyachitra <navya19794@gmail.com>
2022-08-19 18:21:33 +0530

    First Commit

---
+++ src/Pages/LoginPage.java
@@ -0,0 +1,9 @@
+package Pages;
+
+public class LoginPage {
+	
+	public static void main (String b[]) {
+		System.out.println("Login Page");
+	}
+
+}
---
+++ src/tests/HomePageTest.java
@@ -0,0 +1,9 @@
+package tests;
+
+public class HomePageTest {
+	
+	public static void main (String b[]) {
+		System.out.println("Test 1");
+	}
+
+}
